7a4cfc1bf396f1a351ba67b141aced45c891f2a1
frank <fridout106@gmail.com>
2015-12-05 06:33:27 -0600

    Create mininumnumber.js

---
+++ mininumnumber.js
@@ -0,0 +1,13 @@
+function extractChar(array,minNum) {
+
+  for (i=0;i<array.length;i++) {
+    if (array[0] < minNum) {
+      var temp = array.indexOf(array[i])
+      array.splice(temp,1);
+      i--
+    }
+  }
+  return array;
+}
+
+alert(extractChar([1,2,3,4,5,6,7,8,9],5));
